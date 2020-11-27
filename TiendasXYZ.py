138d39025b0dd12ed528dde434a0d04ca375ac63
OMAR BELLO <omar_bello@bhdleon.com.do>
2020-11-27 11:43:54 -0400

    Complete Python Program

--- TiendasXYZ.py
+++ TiendasXYZ.py
@@ -5,7 +5,7 @@ Descuento2 = 0.20
 Descuento3 = 0.15
 
 if Valor<=5000:
-    print("El valor pagado es")
+    print("El valor pagado es: ")
     print(Valor)
     print("El descuento a aplicar es: ")
     print(Descuento1)
@@ -13,7 +13,35 @@ if Valor<=5000:
     ValorDescuento = Valor * Descuento1
     ValorTotal = Valor - ValorDescuento
 
+    print("El valor del descuento es: ")
+    print(ValorDescuento)
     print("El Valor por pagar es: ")
     print(ValorTotal)
 else:
-    if Valor
+    if Valor>5000 and Valor<10000:
+        print("El valor pagado es: ")
+        print(Valor)
+        print("El descuento a aplicar es: ")
+        print(Descuento2)
+
+        ValorDescuento = Valor * Descuento2
+        ValorTotal = Valor - ValorDescuento
+
+        print("El valor del descuento es: ")
+        print(ValorDescuento)
+        print("El Valor por pagar es: ")
+        print(ValorTotal)
+    else:
+        if Valor>10000:
+           print("El valor pagado es: ")
+           print(Valor)
+           print("El descuento a aplicar es: ")
+           print(Descuento3)
+
+           ValorDescuento = Valor * Descuento3
+           ValorTotal = Valor - ValorDescuento
+
+           print("El valor del descuento es: ")
+           print(ValorDescuento)
+           print("El Valor por pagar es: ")
+           print(ValorTotal)
